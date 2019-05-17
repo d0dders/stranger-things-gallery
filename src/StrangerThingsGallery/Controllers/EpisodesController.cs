e28c99240bd9f3f13103a542c7cd63184c621f7a
d0dders <dodders@outlook.com>
2019-05-17 21:34:01 +0100

    Updated Episode Detail view to use ViewBag

--- src/StrangerThingsGallery/Controllers/EpisodesController.cs
+++ src/StrangerThingsGallery/Controllers/EpisodesController.cs
@@ -10,6 +10,16 @@ namespace StrangerThingsGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.SeriesTitle = "Stranger Things";
+            ViewBag.EpisodeNumber = 1;
+            ViewBag.Description = "<p>On his way home from a friend's house, young Will sees something terrifying. Nearby, a sinister secret lurks in the depths of a government lab.</p>";
+            ViewBag.Artists = new string[]
+                {
+                    "Directors: Matt Duffer, Ross Duffer",
+                    "Writers: Matt Duffer, Ross Duffer"
+                };
+
             return View();
             
         }
