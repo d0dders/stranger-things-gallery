503d4a4e1a572f9b855602b6ae7c48671e641a66
d0dders <dodders@outlook.com>
2019-05-18 11:57:47 +0100

    Added Episode and Artist models

---
+++ src/StrangerThingsGallery/Models/Episode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StrangerThingsGallery.Models
+{
+    public class Episode
+    {
+        public int Id { get; set; }
+        public string SeriesTitle { get; set; }
+        public int EpisodeNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+
+    }
+}
