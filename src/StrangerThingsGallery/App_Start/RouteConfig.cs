892a67c3fbadf5316dfdb1ccc462fb3aaabcb16e
d0dders <dodders@outlook.com>
2019-05-19 19:18:03 +0100

    Updated the default route

--- src/StrangerThingsGallery/App_Start/RouteConfig.cs
+++ src/StrangerThingsGallery/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace StrangerThingsGallery
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Episodes", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
